1944b74982253ad6772687d67b155c6aa3086c59
Padd <kjh2@torder.co.kr>
2023-06-16 17:13:33 +0900

    [test] isr test6

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
     next: { revalidate: 10 },
   });
   // The return value is *not* serialized
@@ -14,11 +14,11 @@ async function Coffee() {
 
   console.log(data);
   return (
-    <>
+    <main>
       <h1>Coffee</h1>
       <h2>나는 아아만 마신다</h2>
       <p>그리고 스콘을 좋아하지</p>
-    </>
+    </main>
   );
 }
 
