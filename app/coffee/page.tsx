45ae758a1493ab34cacc7e8116b513cf1ca8c650
Padd <kjh2@torder.co.kr>
2023-06-20 11:57:42 +0900

    [fix] isr page fix

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+    next: {
+      revalidate: 20,
+    },
+  });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
   return res.json();
@@ -10,12 +14,12 @@ async function getData() {
 async function Coffee() {
   const data = await getData();
 
-  console.log(data);
   return (
     <main>
       <h1>Coffee</h1>
       <h2>나는 아아만 마신다</h2>
       <p>그리고 스콘을 좋아하지</p>
+      <p>{data}</p>
     </main>
   );
 }
